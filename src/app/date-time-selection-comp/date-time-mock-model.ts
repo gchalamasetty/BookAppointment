5cba67475612feb9aa3f0de74a4c61d1a8af16d4
Goutham Chalamasetty <chalamasetty.k@gmail.com>
2018-05-24 17:38:14 -0800

    Customize the JSON Object

--- src/app/date-time-selection-comp/date-time-mock-model.ts
+++ src/app/date-time-selection-comp/date-time-mock-model.ts
@@ -11,9 +11,9 @@ export const responseFromAPI = {
     {
       "date": "5/25/2018",
       "dayOfWeek": "FRI",
-      "startTime": "11:00",
-      "endTime": "12:00",
-      "timeSlotDescription": "AM"
+      "startTime": "12:00",
+      "endTime": "01:00",
+      "timeSlotDescription": "PM"
     },
     {
       "date": "5/25/2018",
@@ -26,15 +26,16 @@ export const responseFromAPI = {
       "date": "5/28/2018",
       "dayOfWeek": "MON",
       "startTime": "10:00",
-      "endTime": "12:00",
+      "endTime": "11:00",
       "timeSlotDescription": "AM"
     },
+    
     {
       "date": "5/29/2018",
       "dayOfWeek": "TUE",
-      "startTime": "11:00",
-      "endTime": "12:00",
-      "timeSlotDescription": "AM"
+      "startTime": "12:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
     },
     {
       "date": "5/29/2018",
@@ -98,6 +99,244 @@ export const responseFromAPI = {
       "startTime": "10:00",
       "endTime": "11:00",
       "timeSlotDescription": "AM"
+    },
+    {
+      "date": "7/10/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "7/10/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "04:00",
+      "endTime": "05:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "7/19/2018",
+      "dayOfWeek": "THU",
+      "startTime": "03:00",
+      "endTime": "04:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "7/23/2018",
+      "dayOfWeek": "MON",
+      "startTime": "09:00",
+      "endTime": "10:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "7/23/2018",
+      "dayOfWeek": "MON",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "8/8/2018",
+      "dayOfWeek": "WED",
+      "startTime": "08:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "8/23/2018",
+      "dayOfWeek": "THU",
+      "startTime": "03:00",
+      "endTime": "04:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "8/24/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "8/24/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "03:00",
+      "endTime": "04:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "8/24/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "05:00",
+      "endTime": "06:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "9/11/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "02:00",
+      "endTime": "04:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "9/11/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "03:00",
+      "endTime": "05:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "9/19/2018",
+      "dayOfWeek": "WED",
+      "startTime": "09:00",
+      "endTime": "10:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "9/19/2018",
+      "dayOfWeek": "WED",
+      "startTime": "01:00",
+      "endTime": "03:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "9/21/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "10/5/2018",
+      "dayOfWeek": "THU",
+      "startTime": "09:00",
+      "endTime": "10:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "10/23/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "02:00",
+      "endTime": "03:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "10/23/2018",
+      "dayOfWeek": "TUE",
+      "startTime": "04:00",
+      "endTime": "05:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "10/29/2018",
+      "dayOfWeek": "MON",
+      "startTime": "02:00",
+      "endTime": "03:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "11/14/2018",
+      "dayOfWeek": "WED",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "11/14/2018",
+      "dayOfWeek": "WED",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "11/15/2018",
+      "dayOfWeek": "THU",
+      "startTime": "03:00",
+      "endTime": "05:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "11/19/2018",
+      "dayOfWeek": "MON",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/7/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "12/7/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "02:00",
+      "endTime": "03:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/10/2018",
+      "dayOfWeek": "MON",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "12/10/2018",
+      "dayOfWeek": "MON",
+      "startTime": "04:00",
+      "endTime": "05:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/19/2018",
+      "dayOfWeek": "WED",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/21/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "08:00",
+      "endTime": "09:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "12/28/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "10:00",
+      "endTime": "11:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "12/28/2018",
+      "dayOfWeek": "FRI",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/31/2018",
+      "dayOfWeek": "MON",
+      "startTime": "09:00",
+      "endTime": "10:00",
+      "timeSlotDescription": "AM"
+    },
+    {
+      "date": "12/31/2018",
+      "dayOfWeek": "MON",
+      "startTime": "01:00",
+      "endTime": "02:00",
+      "timeSlotDescription": "PM"
+    },
+    {
+      "date": "12/31/2018",
+      "dayOfWeek": "MON",
+      "startTime": "03:00",
+      "endTime": "04:00",
+      "timeSlotDescription": "PM"
     },
   ]
 }
